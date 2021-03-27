d434e7c57e24459b7000222d6776605352c2f371
Andrew Blomenberg <andrewBlomen@gmail.com>
2021-03-27 12:38:38 -0600

    Updated shopping route to include aisle name

--- src/routes.py
+++ src/routes.py
@@ -8,14 +8,20 @@ from src.models import *
 
 @app.route('/<int:session_id>/shopping')
 def shopping_list(session_id):
+    """
+    :param session_id: The ID of the desired session. TODO potentially read this from a cookie or other storage
+    :return: Json describing a shopping list for the given session.
+        That is, all the ingredients needed to make this session's recipes.
+    """
     ingredient_quantities = db.session.query(Ingredient, func.sum(RecipeIngredient.amount))\
         .join(RecipeIngredient).join(Recipe).join(RecipientSessionRecipe)\
-        .filter(RecipientSessionRecipe.session_id == session_id).group_by(Ingredient.id)
+        .filter(RecipientSessionRecipe.session_id == session_id).group_by(Ingredient.id).order_by(Ingredient.aisle_id)
 
     return jsonify([
         {
             'name': ingredient.name,
             'unit': ingredient.store_unit,
-            'qty': quantity / ingredient.unit_conversion
+            'aisle': ingredient.aisle.name,
+            'qty': quantity / ingredient.unit_conversion,
         } for ingredient, quantity in ingredient_quantities
     ])
