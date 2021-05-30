ec952dd0623e1207a0895d409c3e588bdd5b1ae4
Alison Blomenberg <sheesania@users.noreply.github.com>
2021-05-30 15:51:40 -0600

    link back to browse sessions page

--- client/src/SessionSignup.tsx
+++ client/src/SessionSignup.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 
 interface RouteParams {
@@ -8,8 +9,12 @@ interface RouteParams {
 
 const SessionSignup: React.FC = () => {
   const { sessionId } = useParams<RouteParams>();
+
   return (
     <div>
+      <Link component={RouterLink} variant="body1" to="/">
+        ← Back to session list
+      </Link>
       <Typography variant="h4" component="h1">
         Sign up for a session on {sessionId}
       </Typography>
