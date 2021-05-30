1da9a4f40a58155a4e4990216d1569e53535f403
Alison Blomenberg <sheesania@users.noreply.github.com>
2021-05-30 14:51:32 -0600

    Requests sessions from server and handles errors

--- client/src/ApiTypes.ts
+++ client/src/ApiTypes.ts
@@ -2,3 +2,19 @@ export type Session = {
   id: number;
   date: string;
 };
+
+const parseSession = (json: any): Session => {
+  if (!(json && Number.isInteger(json.id) && typeof json.date === 'string')) {
+    throw new TypeError();
+  }
+
+  return { id: json.id, date: json.date };
+};
+
+export const parseSessionListResponse = (json: any): Session[] => {
+  if (!(json && Array.isArray(json))) {
+    throw new TypeError();
+  }
+
+  return json.map((session) => parseSession(session));
+};
--- client/src/ViewSessions.tsx
+++ client/src/ViewSessions.tsx
@@ -2,15 +2,28 @@ import React, { useCallback, useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Loading from './common/Loading';
 import SessionList from './SessionList';
-import { Session } from './ApiTypes';
+import { Session, parseSessionListResponse } from './ApiTypes';
 
 const ViewSessions: React.FC = () => {
   const [sessions, setSessions] = useState<Session[]>([]);
 
   const getSessions = useCallback<() => Promise<string | void>>(() => {
     return new Promise((resolve, reject) => {
-      setSessions([{ id: 1, date: '11 Jan' }]);
-      resolve();
+      fetch('http://localhost:5000/session')
+        .then((response) => {
+          return response.json();
+        })
+        .then((json: any) => {
+          try {
+            setSessions(parseSessionListResponse(json));
+            resolve();
+          } catch (e) {
+            return Promise.reject();
+          }
+        })
+        .catch((error: Error) => {
+          reject('There was a problem loading the list of sessions');
+        });
     });
   }, [setSessions]);
 
